533c60c1cd3dacc15459b81ad3a3e693179d62b6
Anshul Chaudhary <anshulchaudhary.218@gmail.com>
2025-01-28 12:53:07 -0500

    docker files

--- db_setup/add_users.py
+++ db_setup/add_users.py
@@ -16,11 +16,12 @@ load_dotenv()
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
+db_host_port = os.getenv('DB_HOST_PORT')
 db_name = os.getenv('DB_NAME')
 
 
 # SQLAlchemy setup
-DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_host_port}/{db_name}'
 
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URI)
--- fastapi/db_helper.py
+++ fastapi/db_helper.py
@@ -8,11 +8,12 @@ load_dotenv()
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
+db_host_port = os.getenv('DB_HOST_PORT')
 db_name = os.getenv('DB_NAME')
 
 def get_db_session():
   # SQLAlchemy setup
-  DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+  DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_host_port}/{db_name}'
   engine = create_engine(DATABASE_URI)
   SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
   return SessionLocal
